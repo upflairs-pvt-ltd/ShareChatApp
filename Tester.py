1651731413e683892b243ec180a4d4ba5655d555
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-11-06 18:01:22 +0530

    project infrastructure added

---
+++ Tester.py
@@ -0,0 +1,17 @@
+#### 1.  TESTING LOGGING AND EXCEPTION HANDLING
+from Ai_ShareChat.logger import logging
+import os,sys
+from Ai_ShareChat.exception import ShareChatException
+
+num1 = int(input('enter first number'))
+num2 = int(input('enter secorn number'))
+
+try:
+    logging.info('we are going to deviding ')
+    result = num1 / num2
+    print(f"This is your output :-{result}")
+except Exception as e:
+    logging.info('erorr a gyi')
+    raise ShareChatException(e,sys)
+
+# OUTPUT >>> SUCCESSFULLY TESTED
