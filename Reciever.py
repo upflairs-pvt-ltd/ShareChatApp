112a28e8d096622e916cab325075142ee3ba7fe4
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-11-07 10:49:52 +0530

    message database created

--- Reciever.py
+++ Reciever.py
@@ -1,11 +1,16 @@
 from ShareChat.exception import ShareChatException
 from ShareChat.logger import logging
 from ShareChat.component.chat_message import ChatMessage
+from ShareChat.entity.config_entity import SharingConfig
+from ShareChat.entity.config_entity import MessageConfig
 from ShareChat import utils
 import os,sys,socket,time
 
 #creating Instance of the classes
 Chat_msg_obj = ChatMessage()
+path_obj = SharingConfig()
+message_config_obj = MessageConfig()
+artifact_dir_path = path_obj.artifact_dir
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
 my_ip="0.0.0.0"
@@ -22,6 +27,19 @@ while True :
             logging.info('Dealing with message...')
             print(f"{message[1:]}  >> from --> {ip_address}")
             time.sleep(2)
+
+            #file handling to save the Data of messaging
+            message_artifact_path_with_ip = os.path.join(artifact_dir_path,ip_address,'chat_message')
+            print(message_artifact_path_with_ip)
+            os.makedirs(message_artifact_path_with_ip, exist_ok=True)
+
+            print()
+            message_file_path = os.path.join(message_artifact_path_with_ip , message_config_obj.message_file_name)
+            print(message_file_path)
+            logging.info(f"writing message in Database !")
+            with open(message_file_path,'a+') as message_file:
+                message_file.write('\n'+message[1:])
+
         except Exception as e:
             raise ShareChatException(e,sys)
 
--- ShareChat/entity/config_entity.py
+++ ShareChat/entity/config_entity.py
@@ -5,11 +5,14 @@ import os , sys
 class SharingConfig:
     def __init__(self):
         try:
-            self.artifact_dir = os.getcwd()
+            self.artifact_dir = os.path.join(os.getcwd(),'Artifact')
         except Exception as e:
             raise ShareChatException(e,sys)
 
 
 class MessageConfig:
-    def __init(self,SharingConfig=SharingConfig):
-        self.message_dir = os.paht.join(SharingConfig.artifact_dir,"chat_message")
+    def __init__(self):
+        try:
+            self.message_file_name = 'Inbox.txt'
+        except Exception as e:
+            raise ShareChatException(e,sys)
