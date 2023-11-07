d02aec43251c7b9730002b8295d7ab7c258c9192
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-11-07 15:38:09 +0530

    text file sharing done

--- Reciever.py
+++ Reciever.py
@@ -3,6 +3,7 @@ from ShareChat.logger import logging
 from ShareChat.component.chat_message import ChatMessage
 from ShareChat.entity.config_entity import SharingConfig
 from ShareChat.entity.config_entity import MessageConfig
+from ShareChat.component.Text_file_Process import TextFileProcess
 from ShareChat import utils
 import os,sys,socket,time
 
@@ -11,6 +12,8 @@ Chat_msg_obj = ChatMessage()
 path_obj = SharingConfig()
 message_config_obj = MessageConfig()
 artifact_dir_path = path_obj.artifact_dir
+textfile_class_obj = TextFileProcess()
+
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
 my_ip="0.0.0.0"
@@ -19,7 +22,7 @@ my_address=(my_ip,my_port)
 s.bind(my_address)
 
 while True :
-    recieved_data = s.recvfrom(100)
+    recieved_data = s.recvfrom(250)
     message , ip_address , port = Chat_msg_obj.filter_message_address(RecieveDat=recieved_data)
     Data_label = utils.Data_identifier(Recieve_data_message=message)
     if Data_label == "message":
@@ -30,12 +33,10 @@ while True :
 
             #file handling to save the Data of messaging
             message_artifact_path_with_ip = os.path.join(artifact_dir_path,ip_address,'chat_message')
-            print(message_artifact_path_with_ip)
             os.makedirs(message_artifact_path_with_ip, exist_ok=True)
 
-            print()
+
             message_file_path = os.path.join(message_artifact_path_with_ip , message_config_obj.message_file_name)
-            print(message_file_path)
             logging.info(f"writing message in Database !")
             with open(message_file_path,'a+') as message_file:
                 message_file.write('\n'+message[1:])
@@ -45,9 +46,14 @@ while True :
 
     elif Data_label == 'text-file':
         try:
-            logging.info('Dealing with Text file')
+
+            textfile_artifact_path_with_ip = os.path.join(artifact_dir_path,ip_address,'text_files')
+            text_data = message
+            textfile_class_obj.Data_resampling_write(Recieve_data = text_data , writing_path= textfile_artifact_path_with_ip)
+            logging.info(f"successfully recieved the Text-file !")
         except Exception as e:
             raise ShareChatException(e,sys)
+
 
     else:
         try:
--- Sender.py
+++ Sender.py
@@ -1,13 +1,16 @@
 import os, sys, socket
 from ShareChat.logger import logging
 from ShareChat.exception import ShareChatException
+from ShareChat.component.Text_file_Process import TextFileProcess
+
+textfile_processObj = TextFileProcess()
 
 # establishing connection with UDP
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-
 target_ip="127.0.0.1"
 target_port=1005
 final_target=(target_ip,target_port)
+
 title_message="""
 What do you want to share ?
 0    ---->  MESSAGE
@@ -15,6 +18,8 @@ What do you want to share ?
 2    ---->  IMAGE"""
 print(title_message)
 task_choice = input('Plz enter your choice :- ')
+print()
+
 if task_choice == '0':
     try:
         logging.info(f"we are sending message to :- {final_target}")
@@ -29,9 +34,18 @@ if task_choice == '0':
                 break_condition = False
     except Exception as e:
         raise ShareChatException(e,sys)
+
+
 elif task_choice == '1':
-    # text-file
-    pass
+    try:
+        Data_file_path = input('Plz paste your file path ... ')
+        encrypted_file_data_with_label = textfile_processObj.Data_sampling(file_path= Data_file_path)
+        s.sendto(encrypted_file_data_with_label,final_target)
+        logging.info(f"Text-file Data has been successfully sent to :- {final_target}")
+
+    except Exception as e:
+        raise ShareChatException(e,sys)
+        
 else:
     #images
     pass
